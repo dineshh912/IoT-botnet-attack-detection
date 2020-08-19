69ae49f9c1c9f6c3fa6f49c3fd774a18ad8d6c78
Dineshkumar Sundaram <dineshh912@gmail.com>
2020-08-19 11:10:53 +0530

    eda-added

--- scripts/utils.py
+++ scripts/utils.py
@@ -4,7 +4,7 @@ from glob import glob
 
 
 # This function will read all the csv file and load into dataframe.
-def load_device_data(PATH, EXT, type=3, device_id=False):
+def load_device_data(PATH, EXT):
     """
     Creates a data frame consisting of all the .csv-files in a given directory. The directory should
     be where the unzipped data files are stored. Assumes the file structurce is
@@ -18,8 +18,6 @@ def load_device_data(PATH, EXT, type=3, device_id=False):
         The directory in which the data files are stored. 
     EXT : str
         Extension of the file
-    type : int
-        How many dataframe like to return
         
     Returns
     -------
@@ -29,73 +27,30 @@ def load_device_data(PATH, EXT, type=3, device_id=False):
         consisting of all the mirai data.
     gafgyt_data : pandas data frame
         consisting of all the gafgyt data.
-    df : pandas data frame
-        consisting of all the data combined
     """
     benign_dfs = []
     mirai_dfs = []
     gafgyt_dfs = []
-    if device_id == False:
-        for path, subdir, files in os.walk(PATH):
-            for file in glob(os.path.join(path, EXT)):
-                if 'benign_traffic' in file:
-                    data = pd.read_csv(file)
-                    data['label'] = 'Benign'
-                    data['device'] = file.split('\\')[1]
-                    benign_dfs.append(data)
-                if 'mirai_attacks' in file:
-                    data = pd.read_csv(file)
-                    data['label'] = 'Mirai_'+file.split('\\')[3].split('.')[0]
-                    data['device'] = file.split('\\')[1]
-                    mirai_dfs.append(data)
-                if 'gafgyt_attacks' in file:
-                    data = pd.read_csv(file)
-                    data['label'] = 'Gafgyt_'+file.split('\\')[3].split('.')[0]
-                    data['device'] = file.split('\\')[1]
-                    gafgyt_dfs.append(data)
-        if type == 3:
-            benign_data = pd.concat(benign_dfs, ignore_index=True)
-            mirai_data = pd.concat(mirai_dfs, ignore_index=True)
-            gafgyt_data = pd.concat(gafgyt_dfs, ignore_index=True)
+    for path, subdir, files in os.walk(PATH):
+        for file in glob(os.path.join(path, EXT)):
+            if 'benign_traffic' in file:
+                data = pd.read_csv(file)
+                data['label'] = 'Benign'
+                data['device'] = file.split('\\')[1]
+                benign_dfs.append(data)
+            if 'mirai_attacks' in file:
+                data = pd.read_csv(file)
+                data['label'] = 'Mirai_'+file.split('\\')[3].split('.')[0]
+                data['device'] = file.split('\\')[1]
+                mirai_dfs.append(data)
+            if 'gafgyt_attacks' in file:
+                data = pd.read_csv(file)
+                data['label'] = 'Gafgyt_'+file.split('\\')[3].split('.')[0]
+                data['device'] = file.split('\\')[1]
+                gafgyt_dfs.append(data)
 
-            return benign_data, mirai_data, gafgyt_data
-        if type == 1:
-            dfs = [benign_df, mirai_df, gafgyt_df]
-            df = pd.concat(dfs)
-            
-            return df
-    else:
-        device_list = ['Danmini_Doorbell', 'Ecobee_Thermostat', 
-                        'Ennio_Doorbell', 'Philips_B120N10_Baby_Monitor',
-                        'Provision_PT_737E_Security_Camera', 'Provision_PT_838_Security_Camera',
-                        'Samsung_SNH_1011_N_Webcam', 'SimpleHome_XCS7_1002_WHT_Security_Camera',
-                        'SimpleHome_XCS7_1003_WHT_Security_Camera']
-        for path, subdir, files in os.walk(base_directory):
-            for file in glob(os.path.join(path, file_extension)):
-            if file.split('\\')[1] == device_list[device_id]:
-                if 'benign_traffic' in file:
-                    data = pd.read_csv(file)
-                    data['label'] = 'Benign'
-                    data['device'] = file.split('\\')[1]
-                    benign_dfs.append(data)
-                if 'mirai_attacks' in file:
-                    data = pd.read_csv(file)
-                    data['label'] = 'Mirai_'+file.split('\\')[3].split('.')[0]
-                    data['device'] = file.split('\\')[1]
-                    mirai_dfs.append(data)
-                if 'gafgyt_attacks' in file:
-                    data = pd.read_csv(file)
-                    data['label'] = 'Gafgyt_'+file.split('\\')[3].split('.')[0]
-                    data['device'] = file.split('\\')[1]
-                    gafgyt_dfs.append(data)
-        if type == 3:
-            benign_data = pd.concat(benign_dfs, ignore_index=True)
-            mirai_data = pd.concat(mirai_dfs, ignore_index=True)
-            gafgyt_data = pd.concat(gafgyt_dfs, ignore_index=True)
+    benign_data = pd.concat(benign_dfs, ignore_index=True)
+    mirai_data = pd.concat(mirai_dfs, ignore_index=True)
+    gafgyt_data = pd.concat(gafgyt_dfs, ignore_index=True)
 
-            return benign_data, mirai_data, gafgyt_data
-        if type == 1:
-            dfs = [benign_df, mirai_df, gafgyt_df]
-            df = pd.concat(dfs)
-            
-            return df
+    return benign_data, mirai_data, gafgyt_data
