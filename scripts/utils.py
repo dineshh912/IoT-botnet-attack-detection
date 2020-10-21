1dfb67ce02923bfc1ee77850d4b70e82b3b19832
Dineshkumar Sundaram <dineshh912@gmail.com>
2020-10-21 09:57:09 +0530

    generic-model

--- scripts/utils.py
+++ scripts/utils.py
@@ -117,3 +117,45 @@ def load_data_all(PATH, EXT):
         return device_data
     except Exception as e:
         return str(e)
+
+def load_data_class(PATH, EXT):
+    try:
+        dfs = []
+        for path, subdir, files in os.walk(PATH):
+            for file in glob(os.path.join(path, EXT)):
+                data = pd.read_csv(file)
+                label = file.split("\\")
+                if len(label) == 3:
+                    data['label'] = label[2].split('_')[0]
+                else:
+                    data['label'] = label[2].split('_')[0]
+                data['device'] = file.split('\\')[1]
+                sampled_data = data.sample(frac=0.15)
+                dfs.append(sampled_data)
+        device_data = pd.concat(dfs, ignore_index = True)
+        return device_data
+    
+    except Exception as e:
+        return(str(e))
+
+    
+def load_data_all_multi(PATH, EXT):
+    try:
+        dfs = []
+        for path, subdir, files in os.walk(PATH):
+            for file in glob(os.path.join(path, EXT)):
+                data = pd.read_csv(file)
+                label = file.split('\\')
+                if len(label) == 3:
+                    data['label'] = label[2].split('_')[0]
+                else:
+                    data['label'] = label[2].split('_')[0] + '_' + label[3].split('.')[0]
+                
+                data['device'] = file.split('\\')[1]
+                sampled_data = data.sample(frac=0.15)
+                dfs.append(sampled_data)
+        device_data = pd.concat(dfs, ignore_index = True)
+        
+        return device_data
+    except Exception as e:
+        return str(e)
